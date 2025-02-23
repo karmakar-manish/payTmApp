79259907826abd0516483e5581fd655d63f5041c
karmakar-manish <manishkarmakar106@gmail.com>
2025-02-23 12:27:53 +0530

    Made changes in vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: 5172, // Keep this as your current port
+  },
 })
